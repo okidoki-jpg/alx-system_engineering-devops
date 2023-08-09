82563affadbb46c987ce3412016e4e5085d43dbd
Okuhle <okuhlen.20@gmail.com>
2023-08-09 04:00:36 +0000

    0x16-api_advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Querries to Reddit API """
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Return the number of subscribers """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 200:
+        return r.json().get('data').get('subscribers')
+    return 0
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Recursively query Reddit API """
+
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ Return Reddit hot list recursively """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    params = {'after': after}
+    r = requests.get(url, headers=headers, params=params)
+    if r.status_code == 200:
+        data = r.json().get('data')
+        after = data.get('after')
+        children = data.get('children')
+        for child in children:
+            hot_list.append(child.get('data').get('title'))
+        if after is not None:
+            return recurse(subreddit, hot_list, after)
+        return hot_list

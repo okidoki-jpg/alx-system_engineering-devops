4f90e88a91fea761736ac1df7a36e0af674e99f3
Okuhle Nsibande <81530572+okidoki-jpg@users.noreply.github.com>
2024-01-15 18:37:05 +0200

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Automate error code 500 fix on Apache2 with puppet
-
-exec { 'fix_apache_500_error':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => ['/bin', 'sbin', '/usr/bin', '/usr/sbin'],
-}
--- 0x17-web_stack_debugging_3/README.md
+++
@@ -1,2 +0,0 @@
-# 0x17. Web stack debugging #3
-In this chapter we were taken with analysing an erroneous container and writing a puppet manifest to automate the fix.

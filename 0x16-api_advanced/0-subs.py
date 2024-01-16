834cf3acaca4edf55228e7a02c33c061648d75eb
Okuhle <okuhlen.20@gmail.com>
2024-01-16 11:20:23 +0000

    modded user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,8 @@ import requests
 def number_of_subscribers(subreddit):
     """ Return the number of subscribers """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:110.0) Gecko/20100101 Firefox/110.0.'}
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:110.0)\
+    Gecko/20100101 Firefox/110.0.'}
     r = requests.get(url, headers=headers, allow_redirects=False)
     if r.status_code == 200:
         return r.json().get('data').get('subscribers')

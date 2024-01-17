c2afc4979a80be693cc92ed2d6d4efc7a250fc94
Okuhle <okuhlen.20@gmail.com>
2024-01-17 13:09:54 +0000

    added null check

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,14 +8,10 @@ import requests
 def number_of_subscribers(subreddit):
     """ Return the number of subscribers """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:110.0)\
-    Gecko/20100101 Firefox/110.0.'}
-    try:
-        r = requests.get(url, headers=headers, allow_redirects=False)
-        r.raise_for_status()
-        return r.json()['data']['subscribers']
-    except requests.RequestException as e:
-        print(f"Error: {e}")
-        return 0
-    except KeyError:
-        return 0
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 200:
+        res = r.json().get('data').get('subscribers')
+        if res:
+            return res
+    return 0

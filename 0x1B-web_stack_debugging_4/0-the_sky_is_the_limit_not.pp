56bd6f308dee0d88287e05c65f09d38c1af3504c
Okuhle <okuhlen.20@gmail.com>
2024-01-29 11:19:43 +0000

    0x1B-web_stack_debugging_4 task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,15 +1,30 @@
 # Increase server threshold
-
-# Increase server request limit
-exec { 'increase_server_limit':
-  command => 'sed -i "s/worker_connections 768;/worker_connections 1024;/" /etc/nginx/nginx.conf',
-  path    => '/usr/local/bin/:/bin/'
+# Set worker_processes to 4
+exec { 'set_worker_processes':
+  command => 'sed -i "s/worker_processes [0-9]\+;/worker_processes 4;/" /etc/nginx/nginx.conf',
+  path    => '/usr/local/bin/:/bin/',
+  notify  => Service['nginx'],
 }
 
-# enable-muti_accept
-exec { 'enable-muti_accept':
+# Set worker_connections to 1024
+exec { 'set_worker_connections':
+  command => 'sed -i "s/worker_connections [0-9]\+;/worker_connections 1024;/" /etc/nginx/nginx.conf',
+  path    => '/usr/local/bin/:/bin/',
+  notify  => Service['nginx'],
+}
+
+# Enable multi_accept
+exec { 'enable_multi_accept':
   command => 'sed -i "s/# multi_accept/multi_accept/" /etc/nginx/nginx.conf',
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin/:/bin/',
+  notify  => Service['nginx'],
+}
+
+# Ensure the Nginx service is running
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => [Exec['set_worker_processes', 'set_worker_connections', 'enable_multi_accept']],
 }
 
 # restart nginx

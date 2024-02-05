ea66139f62870633858c3922b1a7d88cd014657c
Okuhle <okuhlen.20@gmail.com>
2024-02-05 12:53:13 +0000

    update limit for holberton user - web_stack_debugging_4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,17 +1,12 @@
-# updatw limits for holberton user
-
+# update limits for holberton user
 # update hard limits for holberton user
 exec { 'add_hard_limits':
-  command => "echo \"holberton hard nofile 5000\" >>
-/etc/security/limits.conf",
-  unless  => "grep -E '^(\\s*#)?\\s*holberton hard.*5000\$' /etc/security/limits.conf",
+  command => 'sed -ir "s/holberton hard .*/holberton hard nofile 1024/" /etc/security/limits.conf',
   path    => ['/bin','/sbin','/usr/bin', '/usr/sbin'],
 }
 
 # update soft limits for holberton user
 exec { 'add_soft_limits':
-  command => "echo \"holberton soft nofile 5000\" >>
-/etc/security/limits.conf",
-  unless  => "grep -E '^(\\s*#)?\\s*holberton soft.*5000\$' /etc/security/limits.conf",
+  command => 'sed -ir "s/holberton soft .*/holberton soft nofile 1024/" /etc/security/limits.conf',
   path    => ['/bin','/sbin','/usr/bin', '/usr/sbin'],
 }
